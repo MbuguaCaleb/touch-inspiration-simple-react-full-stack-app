bd63693e5496389387033a647a5870344ae3d2d2
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-01-20 19:02:56 +0300

    completing-creation-of-create-user-component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,45 @@ import createUserComponent from "./components/createUserComponent";
 import editUserComponet from "./components/editUserComponent";
 import usersListComponent from "./components/usersListComponent";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logoTwo from "./logoTwo.png";
 
-export class App extends Component {
+class App extends Component {
   render() {
     return (
       <Router>
         <div className='container'>
-          <h2>Touch-Inspiration User Application</h2>
+          <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <a
+              href='navbar-brand'
+              href='https://touchinspiration.com/'
+              target='_blank'
+            >
+              <img
+                src={logoTwo}
+                width='30'
+                height='30'
+                alt='Touch inspiration User Application'
+              />
+            </a>
+            <Link to='/' className='navbar-brand'>
+              Touch Inspiration User Application
+            </Link>
+
+            <div className='collapase navbar-collapse'>
+              <ul className='navbar-nav mr-auto'>
+                <li className='navbar-item'>
+                  <Link to='/' className='nav-link'>
+                    UsersList
+                  </Link>
+                </li>
+                <li className='navbar-item'>
+                  <Link to='/create' className='nav-link'>
+                    Create User
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
           <Route path='/' exact component={usersListComponent} />
           <Route path='/edit/:id' component={editUserComponet} />
           <Route path='/create' component={createUserComponent} />
--- src/components/createUserComponent.js
+++ src/components/createUserComponent.js
@@ -1,10 +1,118 @@
 import React, { Component } from "react";
 
 export class createUserComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    //bounding event methods to the class.
+    this.onChangeUserName = this.onChangeUserName.bind(this);
+    this.onChangeOccupation = this.onChangeOccupation.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangeBio = this.onChangeBio.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.state = {
+      name: "",
+      occupation: "",
+      email: "",
+      bio: ""
+    };
+  }
+
+  //class Methods
+  onChangeUserName = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+  //class Methods
+  onChangeEmail = e => {
+    this.setState({
+      email: e.target.value
+    });
+  };
+  onChangeBio = e => {
+    this.setState({
+      bio: e.target.value
+    });
+  };
+  //class Methods
+  onChangeOccupation = e => {
+    this.setState({
+      occupation: e.target.value
+    });
+  };
+
+  //Method to Handle Submit
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    console.log(`Form submitted`);
+    console.log(`name:${this.state.name}`);
+    console.log(`occupation:${this.state.occupation}`);
+    console.log(`email:${this.state.email}`);
+    console.log(`bio:${this.state.bio}`);
+
+    //Resetting to empty upon submit
+    this.setState({
+      name: "",
+      occupation: "",
+      email: "",
+      bio: ""
+    });
+  };
   render() {
     return (
-      <div>
-        <h1>Welcome to Create User Component</h1>
+      <div style={{ marginTop: 20 }}>
+        <h3>Create New User</h3>
+
+        <form onSubmit={this.onSubmit}>
+          <div className='form-group'>
+            <label>Name:</label>
+            <input
+              type='text'
+              className='form-control'
+              value={this.state.name}
+              onChange={this.onChangeUserName}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label>Email:</label>
+            <input
+              type='text'
+              className='form-control'
+              value={this.state.email}
+              onChange={this.onChangeEmail}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Occupation:</label>
+            <input
+              type='text'
+              className='form-control'
+              value={this.state.occupation}
+              onChange={this.onChangeOccupation}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Bio:</label>
+            <input
+              type='text'
+              className='form-control'
+              value={this.state.bio}
+              onChange={this.onChangeBio}
+            />
+          </div>
+
+          <div className='form-group'>
+            <input
+              type='submit'
+              value='Create User'
+              className='btn btn-primary'
+            />
+          </div>
+        </form>
       </div>
     );
   }
